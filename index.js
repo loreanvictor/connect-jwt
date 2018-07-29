bfe3db43851b24c9edee0e4b1cf049c8e76602b5
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2018-07-29 14:06:05 +0200

    updated platform version, added aliases

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ module.exports.platform = {
   config: {
     nodes : {
       native : ['sign','verify']
+    },
+    aliases: {
+      '/token/sign': '/jwt/sign',
+      '/token/verify': '/jwt/verify',
     }
   }
 }
