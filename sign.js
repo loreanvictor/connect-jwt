d47e7482ad34123a79cc66a6818f6a86c3494c1f
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2020-02-27 17:59:19 +0100

    Added options to the sign method

--- sign.js
+++ sign.js
@@ -5,18 +5,19 @@ const platform = require('connect-platform');
 platform.core.node({
   path: '/jwt/sign',
   public: false,
-  inputs: ['payload', 'key'],
+  inputs: ['payload', 'key', 'options'],
   outputs: ['token'],
   hints: {
     node: 'signs and encrypts given <span class="hl-blue">payload</span>.',
     inputs: {
       payload: 'the payload to sign',
       key: 'the key to sign with',
+      key: 'additional options for signing',
     },
     outputs: {
       token: 'the signed and encrypted jwt token containing the given <span class="hl-blue">payload</span>.'
     },
   }
 }, (inputs, output, control) => {
-  jwt.sign(inputs.payload, inputs.key, {}, (_, token) => output('token', token));
+  jwt.sign(inputs.payload, inputs.key, inputs.options, (_, token) => output('token', token));
 });
